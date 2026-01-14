b1336197b534714acd4fd8eff679785433d8464e
curiosity <2681446651@qq.com>
2026-01-15 02:43:25 +0800

    first time commit

---
+++ src/halo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    int a = 1 + 2; // to test Prettier extension
+    std::cout << "halo" << std::endl;
+    std::cout << a;
+}
---
+++ src/just_a_dir/cute.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    int a = 1 + 2; // to test Prettier extension
+    std::cout << "Nia is cute." << std::endl;
+    std::cout << a;
+}
